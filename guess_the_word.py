4b8fe210d6007421b9e803c966ec227ff2e8f3e0
cdeborja <cdeborja@gmail.com>
2016-10-22 15:51:33 -0400

    Create logic for guessing

--- guess_the_word.py
+++ guess_the_word.py
@@ -19,14 +19,34 @@ for letter in answer:
 
 
 while guesses_remaining > 0:
+    print "--- Letter Bank ---"
+    print valid_letters
+    print "-------------------"
     print shown
+
+    if find_element_in_string("_",shown) == None:
+        print "Figured out word! Congratulations!"
+        break
     print "Guesses Remaining: " + str(guesses_remaining)
     guess = raw_input("Guess a letter: ").lower()
     os.system('clear')
+
     if len(guess) > 1 or len(guess) == 0 or find_element_in_string(guess,valid_letters) == None:
         print "Please enter valid character"
     elif find_element_in_string(guess, answer) >= 0:
-        print "found a letter"
+        print "Found a letter!"
+        valid_letters = valid_letters.replace(guess, "")
+        shown_arr = list(shown)
+        answer_arr = list(answer)
+        for count, elem in enumerate(answer_arr):
+            if elem == guess:
+                shown_arr[count] = guess
+        shown = "".join(shown_arr)
+        answer = "".join(answer_arr)
     else:
         print "Letter doesn't exist"
+        valid_letters = valid_letters.replace(guess, "")
         guesses_remaining-= 1
+
+print "Secret word was: " + answer
+print "Sorry, you lose!"
