4c1323d4406e097dbbead2b55b2a0c960745c67a
cdeborja <cdeborja@gmail.com>
2016-10-24 11:27:55 -0400

    Add choice to allow for get random word or user input

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,5 +1,11 @@
 import os
 import string
+import random
+
+os.system('clear')
+
+with open("dictionary.txt") as f:
+    content = f.readlines()
 
 def find_element_in_string(element, string):
     try:
@@ -10,7 +16,19 @@ def find_element_in_string(element, string):
 
 guesses_remaining = 5
 valid_letters = string.lowercase
-answer = raw_input("Input one secret word: ").lower()
+choice = ""
+
+while choice != "m" and choice != "r":
+    choice = raw_input("Enter [m]ystery word or get [r]andom word: ").lower()
+    os.system('clear')
+
+
+if choice == "m":
+    answer = raw_input("Input one secret word: ").lower()
+else:
+    answer = (random.choice(content))
+
+
 shown = ""
 os.system('clear')
 
@@ -48,5 +66,6 @@ while guesses_remaining > 0:
         valid_letters = valid_letters.replace(guess, "")
         guesses_remaining-= 1
 
-print "Secret word was: " + answer
-print "Sorry, you lose!"
+if guesses_remaining == 0:
+    print "Secret word was: " + answer
+    print "Sorry, you lose!"
